e9cc01f4af74ddbd3b81edbc176056878bb5ebd0
darthjee <darthjee@gmail.com>
2020-09-21 16:20:16 +0200

    Add User migration

---
+++ source/db/migrate/20200921122009_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users, force: :cascade do |t|
+      t.string :name,               null: false
+      t.string :login,              null: false
+      t.string :email,              null: false
+      t.string :encrypted_password, null: false
+      t.string :salt,               null: false
+
+      t.timestamps
+
+      t.index :email, unique: true
+      t.index :login, unique: true
+    end
+  end
+end
