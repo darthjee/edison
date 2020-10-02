6b8182fff96990d2a09d9c43c3067ad73e4374a5
darthjee <darthjee@gmail.com>
2020-10-02 11:37:04 +0200

    Add download route

--- source/app/controllers/user_files_controller.rb
+++ source/app/controllers/user_files_controller.rb
@@ -7,6 +7,25 @@ class UserFilesController < ApplicationController
   resource_for :user_file, only: %i[index show]
   before_action :check_logged!
 
+  def download
+    response.set_header(
+      'Content-Type', Rack::Mime::MIME_TYPES[user_file.extension]
+    )
+    response.set_header(
+      'ETag', user_file.md5
+    )
+    response.set_header(
+      'Content-Disposition', "attachment; filename=\"#{user_file.name}\""
+    )
+    response.set_header("Content-Length", user_file.size)
+
+    user_file.read do |chunk|
+      response.stream.write chunk
+    end
+  ensure
+    response.stream.close
+  end
+
   private
 
   def user_files
